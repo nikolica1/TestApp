b58900a73e9e4f898b374a81b55dcbc180e17b92
Nikola Bajagic <nikolicabajagic1@gmail.com>
2025-03-28 11:28:38 +0100

    implement and test GoWavey SDK

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,80 @@
-import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useCallback, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+} from "react-native";
+import {
+  initializeSdk,
+  showTrophyCase,
+  updateActivity,
+} from "gowavey-react-native-sdk";
 
-export default function HomeScreen() {
-  const [text, setText] = useState('');
+export default function App() {
+  const [isSdkInitialized, setIsSdkInitialized] = useState(false);
+  const [value, setValue] = useState("");
 
-  const handlePress = () => {
-    alert(`Uneto: ${text}`);
-  };
+  const initSdk = useCallback(async () => {
+    try {
+      await initializeSdk("eS8wmBobAEbN63jvDWZQyY", "Andrej_Noubis_2025");
+      setIsSdkInitialized(true);
+    } catch (error) {
+      console.error("SDK initialization failed", error);
+    }
+  }, []);
+
+  const showTrophy = useCallback(async () => {
+    try {
+      await showTrophyCase("hELDrquYXu49W1wcHL8dGe");
+    } catch (error) {
+      console.error("Show trophy case failed", error);
+    }
+  }, []);
+
+  const handleUpdateActivity = useCallback(async () => {
+    try {
+      if (value) {
+        await updateActivity("v8H78W2EmEvxeFpKnYK4dx", parseFloat(value), true);
+      }
+    } catch (error) {
+      console.error("Update activity failed", error);
+    }
+  }, [value]);
 
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <TouchableOpacity style={styles.button} onPress={initSdk}>
+        <Text>Initialize SDK</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.button, !isSdkInitialized && styles.disabledButton]}
+        onPress={showTrophy}
+        disabled={!isSdkInitialized}
+      >
+        <Text>Show Trophy</Text>
+      </TouchableOpacity>
+
       <TextInput
         style={styles.input}
-        value={text}
-        onChangeText={setText}
+        placeholder="Enter Activity Value"
+        value={value}
+        onChangeText={setValue}
+        keyboardType="numeric"
       />
-      <Button title="Send" onPress={handlePress} />
+
+      <TouchableOpacity
+        style={[styles.button, !isSdkInitialized && styles.disabledButton]}
+        onPress={handleUpdateActivity}
+        disabled={!isSdkInitialized}
+      >
+        <Text>Update Activity</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,16 +82,26 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', 
-    alignItems: 'center',     
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
+  },
+  button: {
+    margin: 10,
+    padding: 10,
+    backgroundColor: "#ddd",
+    borderRadius: 5,
+  },
+  disabledButton: {
+    backgroundColor: "#eee",
   },
   input: {
     height: 40,
-    width: 100,
+    borderColor: "gray",
     borderWidth: 1,
-    padding: 10,
-    marginBottom: 20,
-    borderRadius: 5,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: "100%",
   },
 });
